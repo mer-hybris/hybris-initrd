0ecd2d714245fa9201a6c7b5b5f2f2bdbaf08d77
Kalle Jokiniemi <kalle.jokiniemi@jolla.com>
2015-04-02 18:46:54 +0300

    Initial packaging Jolla Tablet kernel boot image.
    This initial commit contains new packaging for Jolla Tablet kernel
    boot image with moslo-build (hw-ramdisk) based initrd. Features
    include:
    
    * Support for LVM based rootfs with separate LVs for root and home
    * First boot resizing of PV, LVs, and ext4 filesystems
    * Support for legacy single ext4 rootfs
    * New tools in initrd: lvm2, e2fsck, resize2fs, yamui, and mkfs.ext4
    
    [tbj] Initial packaging Jolla Tablet kernel boot image. Fixes JB#27687
    [lvm] Support for LVM based filesystem. Contributes to JB#25193
    
    Signed-off-by: Kalle Jokiniemi <kalle.jokiniemi@jolla.com>

---
+++ mksfosinitrd.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Simple script to generate inird rootfs for Sailfish OS running on Jolla Tablet
+
+# Add your tools here. They need to be present in your sb2 target.
+TOOL_LIST="sbin/* debug-init /sbin/e2fsck /usr/sbin/lvm /usr/bin/yamui /sbin/resize2fs /sbin/mkfs.ext4"
+
+# The default init script
+DEF_INIT="jolla-init"
+
+OLD_DIR=$(pwd)
+TMP_DIR=/tmp/sfosinitrd
+
+if test -z "$SBOX_SESSION_DIR"; then
+	echo "Please run this script inside scratchbox2 target."
+	exit 1
+fi
+
+check_files()
+{
+	local FILES=$1
+	for f in $FILES; do
+		if test ! -e "$f"; then
+			echo "File \"$f\" does not exist!"
+			echo "Please install required RPM package or add \"$f\" manually"
+			return 1
+		fi
+	done
+	return 0
+}
+
+check_files "$TOOL_LIST" || exit 1
+
+rm -rf "$TMP_DIR" initramfs
+mkdir "$TMP_DIR"
+cd "$TMP_DIR"
+
+# Copy local files to be added to initrd. If you add more, add also to TOOL_LIST.
+cp -a "$OLD_DIR"/sbin .
+cp -a "$OLD_DIR"/debug-init .
+# TODO, pictures for yamui: cp -a "$OLD_DIR"/res .
+
+# Create the ramdisk
+initialize-ramdisk.sh -w ./ -t "$TOOL_LIST" -i "$OLD_DIR"/"$DEF_INIT" || exit 1
+moslo-build.sh -w ./ -v 2.0 || exit 1
+cd "$OLD_DIR"
+cp -a "$TMP_DIR"/rootfs initramfs
+cp -a "$TMP_DIR"/rootfs.cpio.gz .
+
+rm -rf "$TMP_DIR"
+
