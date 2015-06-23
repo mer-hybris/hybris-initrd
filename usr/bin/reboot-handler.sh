1a7d809ccd71f1c74a53498f5208bf1f46d20b26
Kalle Jokiniemi <kalle.jokiniemi@jolla.com>
2015-06-23 20:20:37 +0300

    [initrd] Remove led-helper.sh. Contributes to JB#29876
    Jolla Tablet does not have a LED support. Remove the led-helper.sh
    script.
    
    Signed-off-by: Kalle Jokiniemi <kalle.jokiniemi@jolla.com>

--- usr/bin/reboot-handler.sh
+++ usr/bin/reboot-handler.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 echo "Rebooting..."
 
-# Start red led to indicate that we are shutting down
-/usr/bin/led-helper.sh 255 0 0 1000 0
-
-# Give some time for red led to show to user
-sleep 2
-
 sync
 
 reboot -f
