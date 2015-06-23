c6ab64b845e5ffd0e6754537368f43bf41f4287b
Kalle Jokiniemi <kalle.jokiniemi@jolla.com>
2015-06-23 20:13:32 +0300

    [recovery] Device specific check for power key device. Contributes to JB#29876
    The event device nodes can change and as we haven't yet
    reached final hardware configuration. Added a dynamic search
    for the correct power key event device to symlink it into
    /dev/powerkey. Changed powerkey-handler.sh to use the symlink
    instead of hard coded device.
    
    Signed-off-by: Kalle Jokiniemi <kalle.jokiniemi@jolla.com>

--- usr/bin/powerkey-handler.sh
+++ usr/bin/powerkey-handler.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  key=$(/sbin/evkey -d -t 3000 /dev/input/event6)
+  key=$(/sbin/evkey -d -t 3000 /dev/powerkey)
   
   if [ "x${key}" == "x116" ]; then
     echo "Powerkey pressed, rebooting the device.."
