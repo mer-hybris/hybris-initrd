7a21c6019d1f2d6f98e640930f8659c45d98171c
Kalle Jokiniemi <kalle.jokiniemi@jolla.com>
2015-06-23 18:31:03 +0300

    [recovery] Fix power key event device. Contributes to JB#29876
    The event device for power key on Jolla Tablet is event6.
    Let's use that.
    
    Signed-off-by: Kalle Jokiniemi <kalle.jokiniemi@jolla.com>

--- usr/bin/powerkey-handler.sh
+++ usr/bin/powerkey-handler.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  key=$(/sbin/evkey -d -t 3000 /dev/input/event3)
+  key=$(/sbin/evkey -d -t 3000 /dev/input/event6)
   
   if [ "x${key}" == "x116" ]; then
     echo "Powerkey pressed, rebooting the device.."
