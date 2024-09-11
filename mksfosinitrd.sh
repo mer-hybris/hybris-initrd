60068634f8c4cd2ac61f643c7421994a806bb34f
Matti Lehtimäki <matti.lehtimaki@jolla.com>
2024-09-11 15:12:46 +0000

    [initrd] Add support for creating custom vendor_boot image
    Remove not needed defattr.

--- mksfosinitrd.sh
+++ mksfosinitrd.sh
@@ -42,6 +42,9 @@ RECOVERY_FILES="				\
 	/usr/sbin/sshd				\
 	/usr/sbin/cryptsetup"
 
+# These tools will be included to vendor_boot initrd only.
+VENDOR_BOOT_FILES=""
+
 # The sshd config file and keys must be accessible by files owner only.
 # Git doesn't preserve full file permissions.
 FIX_FILE_PERMISSIONS="				\
@@ -64,7 +67,11 @@ fi
 
 shift
 
-if test x"$1" = x"recovery"; then
+if test x"$1" = x"vendor_boot"; then
+	# Does not include the files from boot image tools
+	TOOL_LIST="$VENDOR_BOOT_FILES_FILES $(cat vendor_boot.files 2> /dev/null)"
+	DEF_INIT="jolla-init"
+elif test x"$1" = x"recovery"; then
 	TOOL_LIST="$TOOL_LIST $RECOVERY_FILES $(cat recovery.files 2> /dev/null)"
 	DEF_INIT="recovery-init"
 else
@@ -104,23 +111,50 @@ rm -rf "$TMP_DIR"
 mkdir "$TMP_DIR"
 cd "$TMP_DIR"
 
-# Copy local files to be added to initrd. If you add more, add also to TOOL_LIST.
-cp -a "$OLD_DIR"/sbin .
-mkdir -p usr/bin
-cp -a "$OLD_DIR"/usr/bin/functions.sh usr/bin
-mkdir -p res/images
-cp -a /usr/share/initrd-logos/* res/images
-mkdir -p etc
-cp -a "$OLD_DIR"/etc/sysconfig etc
+if test x"$1" != x"vendor_boot"; then
+	# Copy local files to be added to initrd. If you add more, add also to TOOL_LIST.
+	cp -a "$OLD_DIR"/sbin .
+	mkdir -p usr/bin
+	cp -a "$OLD_DIR"/usr/bin/functions.sh usr/bin
+	mkdir -p res/images
+	cp -a /usr/share/initrd-logos/* res/images
+	mkdir -p etc
+	cp -a "$OLD_DIR"/etc/sysconfig etc
+fi
 
 # Copy recovery files
 if test x"$1" = x"recovery"; then
 	cp -a "$OLD_DIR"/usr/ "$OLD_DIR"/etc/ -t ./
 fi
 
+# Copy vendor_boot files
+if test x"$1" = x"vendor_boot"; then
+	if [ -d "$OLD_DIR"/lib/vendor_boot_modules ]; then
+		mkdir -p lib/modules
+		cp -a "$OLD_DIR"/lib/vendor_boot_modules/* lib/modules
+	fi
+fi
+
 # Create the ramdisk
-initialize-ramdisk.sh -w ./ -t "$TOOL_LIST" -i "$OLD_DIR"/"$DEF_INIT" || exit 1
-moslo-build.sh -w ./ -v 2.0 $MOSLO_COMPRESSION_PARAM || exit 1
+if test x"$1" != x"vendor_boot"; then
+	initialize-ramdisk.sh -w ./ -t "$TOOL_LIST" -i "$OLD_DIR"/"$DEF_INIT" || exit 1
+	moslo-build.sh -w ./ -v 2.0 $MOSLO_COMPRESSION_PARAM || exit 1
+else
+	# Custom creation of vendor_boot ramdisk to prevent useless files from being added
+	# to ramdisk by initialize-ramdisk.sh and moslo-build.sh which assume basic
+	# filesystem structure and some files are always needed which is not the case
+	# for vendor_boot. In the future initialize-ramdisk.sh and moslo-build.sh could
+	# be adjusted to support also vendor_boot use case.
+	WORK_DIR=./
+	gen_initramfs_list.sh -o $WORK_DIR/rootfs.cpio -u squash -g squash $WORK_DIR || exit 1
+	if test x"$COMPRESSION_FORMAT" = x"lz4"; then
+		lz4 -f -l -12 --favor-decSpeed $WORK_DIR/rootfs.cpio $WORK_DIR/rootfs.cpio.lz4 || exit 1
+		echo Build is ready at $WORK_DIR/rootfs.cpio.lz4
+	else
+		gzip -f  $WORK_DIR/rootfs.cpio || exit 1
+		echo Build is ready at $WORK_DIR/rootfs.cpio.gz
+	fi
+fi
 cd "$OLD_DIR"
 cp -a "$TMP_DIR"/rootfs.cpio.$COMPRESSION_FORMAT .
 
