82560e9bf274bd17d5f370cfcf06904a451b6558
Pami Ketolainen <pami.ketolainen@jolla.com>
2025-09-30 23:16:02 +0300

    [mksfosinitrd] Cleanup
    * Cleanup the commandline arguments handling
    * Use [] instead of test in the conditionals
    * Remove unnecessary whitespace and line brek escapes
    * Rename jolla-init to normal-init to align with the init types.

--- mksfosinitrd.sh
+++ mksfosinitrd.sh
@@ -3,85 +3,111 @@
 # Simple script to generate inird rootfs for Sailfish OS devices.
 # NOTE: if you run this locally, please do it inside Scratchbox 2 target.
 
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <lib path> <compression format> [<init type>]"
+	exit 1
+fi
+case "$1" in
+	lib|lib64)
+		LIB=$1
+		;;
+	*)
+		echo "Invalid lib path '$1' (lib, lib64)"
+		exit 1
+		;;
+esac
+case "$2" in 
+	"lz4")
+		COMPRESSION_FORMAT=lz4
+		MOSLO_COMPRESSION_PARAM=-l
+		;;
+	"gz")
+		COMPRESSION_FORMAT=gz
+		;;
+	*)
+		echo "Invalid compression format '$2' (gz, lz4)"
+		exit 1
+		;;
+esac
+INIT_TYPE=${3-normal}
+
+
 # Add your tools here. They need to be present in your sb2 target.
 # These tools will be included both to normal and recovery initrd.
-TOOL_LIST="					\
-	etc/sysconfig/*				\
-	res/images/*				\
-	sbin/*					\
-	usr/bin/functions.sh			\
-	/sbin/e2fsck				\
-	/sbin/factory-reset-external		\
-	/sbin/factory-reset-lvm			\
-	/sbin/find-mmc-bypartlabel		\
-	/usr/sbin/lvm				\
-	/sbin/mkfs.ext4				\
-	/sbin/resize2fs				\
-	/usr/bin/pigz				\
-	/usr/bin/xz				\
-	/usr/bin/yamui				\
-	/usr/bin/yamui-powerkey			\
+TOOL_LIST="
+	etc/sysconfig/*
+	res/images/*
+	sbin/*
+	usr/bin/functions.sh
+	/sbin/e2fsck
+	/sbin/factory-reset-external
+	/sbin/factory-reset-lvm
+	/sbin/find-mmc-bypartlabel
+	/usr/sbin/lvm
+	/sbin/mkfs.ext4
+	/sbin/resize2fs
+	/usr/bin/pigz
+	/usr/bin/xz
+	/usr/bin/yamui
+	/usr/bin/yamui-powerkey
 	/usr/bin/yamui-screensaverd"
 
 # These tools will be included to recovery initrd only.
-RECOVERY_FILES="				\
-	etc/fstab				\
-	etc/group				\
-	etc/gshadow				\
-	etc/passwd				\
-	etc/profile				\
-	etc/shadow				\
-	etc/ssh/*				\
-	etc/udhcpd.conf				\
-	usr/bin/*				\
-	/etc/nsswitch.conf			\
-	/$1/libnss_compat.so.2			\
-	/$1/libnss_files.so.2			\
-	/usr/bin/scp				\
-	/usr/libexec/openssh/sftp-server	\
-	/usr/libexec/openssh/sshd-session	\
-	/usr/sbin/sshd				\
+RECOVERY_FILES="
+	etc/fstab
+	etc/group
+	etc/gshadow
+	etc/passwd
+	etc/profile
+	etc/shadow
+	etc/ssh/*
+	etc/udhcpd.conf
+	usr/bin/*
+	/etc/nsswitch.conf
+	/$LIB/libnss_compat.so.2
+	/$LIB/libnss_files.so.2
+	/usr/bin/scp
+	/usr/libexec/openssh/sftp-server
+	/usr/libexec/openssh/sshd-session
+	/usr/sbin/sshd
 	/usr/sbin/cryptsetup"
 
 # These tools will be included to vendor_boot initrd only.
 VENDOR_BOOT_FILES=""
 
-# The sshd config file and keys must be accessible by files owner only.
-# Git doesn't preserve full file permissions.
-FIX_FILE_PERMISSIONS="				\
-	etc/ssh/ssh_host_ed25519_key		\
-	etc/ssh/ssh_host_rsa_key		\
-	etc/ssh/sshd_config"
-
-chmod 0600 $FIX_FILE_PERMISSIONS
-
 TOOL_LIST="$TOOL_LIST $(cat tools.files 2> /dev/null)"
 
-shift
-
-if test x"$1" = x"lz4"; then
-	COMPRESSION_FORMAT=lz4
-	MOSLO_COMPRESSION_PARAM=-l
-else
-	COMPRESSION_FORMAT=gz
-fi
-
-shift
-
-if test x"$1" = x"vendor_boot"; then
-	# Does not include the files from boot image tools
-	TOOL_LIST="$VENDOR_BOOT_FILES_FILES $(cat vendor_boot.files 2> /dev/null)"
-	DEF_INIT="jolla-init"
-elif test x"$1" = x"recovery"; then
-	TOOL_LIST="$TOOL_LIST $RECOVERY_FILES $(cat recovery.files 2> /dev/null)"
-	DEF_INIT="recovery-init"
-else
-	# The default init script
-	DEF_INIT="jolla-init"
-fi
+case "$INIT_TYPE" in
+	vendor_boot)
+		# Does not include the files from boot image tools
+		TOOL_LIST="$VENDOR_BOOT_FILES_FILES $(cat vendor_boot.files 2> /dev/null)"
+		DEF_INIT="normal-init"
+		;;
+	recovery)
+		TOOL_LIST="$TOOL_LIST $RECOVERY_FILES $(cat recovery.files 2> /dev/null)"
+		DEF_INIT="recovery-init"
+		;;
+	normal)
+		# The default init script
+		DEF_INIT="normal-init"
+		;;
+	*)
+		echo "Invalid init type '$INIT_TYPE' (normal, recovery, vendor_boot)"
+		exit 1
+		;;
+esac
 
 # Remove duplicates.
 TOOL_LIST="$(echo $TOOL_LIST | sort | uniq)"
+
+# The sshd config file and keys must be accessible by files owner only.
+# Git doesn't preserve full file permissions.
+FIX_FILE_PERMISSIONS="
+	etc/ssh/ssh_host_ed25519_key
+	etc/ssh/ssh_host_rsa_key
+	etc/ssh/sshd_config"
+
+chmod 0600 $FIX_FILE_PERMISSIONS
 
 set -e
 
@@ -92,7 +118,7 @@ check_files()
 {
 	local FILES=$1
 	for f in $FILES; do
-		if test ! -e "$f"; then
+		if [ ! -e "$f" ]; then
 			# skip empty images folder
 			if [ "$f" = "res/images/*" ]; then
 				continue
@@ -112,7 +138,7 @@ rm -rf "$TMP_DIR"
 mkdir "$TMP_DIR"
 cd "$TMP_DIR"
 
-if test x"$1" != x"vendor_boot"; then
+if [ "$INIT_TYPE" != "vendor_boot" ]; then
 	# Copy local files to be added to initrd. If you add more, add also to TOOL_LIST.
 	cp -a "$OLD_DIR"/sbin .
 	mkdir -p usr/bin
@@ -124,12 +150,12 @@ if test x"$1" != x"vendor_boot"; then
 fi
 
 # Copy recovery files
-if test x"$1" = x"recovery"; then
+if [ "$INIT_TYPE" = "recovery" ]; then
 	cp -a "$OLD_DIR"/usr/ "$OLD_DIR"/etc/ -t ./
 fi
 
 # Copy vendor_boot files
-if test x"$1" = x"vendor_boot"; then
+if [ "$INIT_TYPE" = "vendor_boot" ]; then
 	if [ -d "$OLD_DIR"/lib/vendor_boot_modules ]; then
 		mkdir -p lib/modules
 		cp -a "$OLD_DIR"/lib/vendor_boot_modules/* lib/modules
@@ -137,7 +163,7 @@ if test x"$1" = x"vendor_boot"; then
 fi
 
 # Create the ramdisk
-if test x"$1" != x"vendor_boot"; then
+if [ "$INIT_TYPE" != "vendor_boot" ]; then
 	initialize-ramdisk.sh -w ./ -t "$TOOL_LIST" -i "$OLD_DIR"/"$DEF_INIT" || exit 1
 	moslo-build.sh -w ./ -v 2.0 $MOSLO_COMPRESSION_PARAM || exit 1
 else
@@ -148,7 +174,7 @@ else
 	# be adjusted to support also vendor_boot use case.
 	WORK_DIR=./
 	gen_initramfs_list.sh -o $WORK_DIR/rootfs.cpio -u squash -g squash $WORK_DIR || exit 1
-	if test x"$COMPRESSION_FORMAT" = x"lz4"; then
+	if [ "$COMPRESSION_FORMAT" = "lz4" ]; then
 		lz4 -f -l -12 --favor-decSpeed $WORK_DIR/rootfs.cpio $WORK_DIR/rootfs.cpio.lz4 || exit 1
 		echo Build is ready at $WORK_DIR/rootfs.cpio.lz4
 	else
